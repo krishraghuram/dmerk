295fc55975b4319f59f95769116371ffd2723c02
Raghuram Krishnaswami <krish.raghuram@gmail.com>
2023-03-04 01:46:30 -0800

    Updated noxfile

--- noxfile.py
+++ noxfile.py
@@ -6,26 +6,45 @@ import nox
 # Usage: nox --session <session-name>
 # Example: nox --session lint
 
+
 @nox.session()
 def lint(session):
     session.install("pyflakes")
     session.run("pyflakes", "dmerk")
 
+
 @nox.session()
 def format(session):
     session.install("black")
     out = session.run("black", "dmerk", silent=True)
-    match = re.search(r"(\d*?)(?: files? reformatted, )?(\d*?) files? left unchanged.", out)
-    if int(match.groups()[0] or 0)>0:  # https://stackoverflow.com/a/46254991/5530864
-        raise Exception(f"black has formatted {int(match.groups()[0])} files, please check them")
+    match = re.search(
+        r"(\d*?)(?: files? reformatted, )?(\d*?) files? left unchanged.", out
+    )
+    if int(match.groups()[0] or 0) > 0:  # https://stackoverflow.com/a/46254991/5530864
+        raise Exception(
+            f"black has formatted {int(match.groups()[0])} files, please check them"
+        )
 
-@nox.session(python=["3.8", "3.10", "3.11"])
+
+@nox.session(
+    python=[
+        "/usr/bin/python3.10",
+        "/home/raghuram/Workspace/bin/Python-3.9.16/python",
+        "/home/raghuram/Workspace/bin/Python-3.11.2/python",
+        "/home/raghuram/Workspace/bin/pypy3.9-v7.3.11-linux64/bin/pypy",
+    ]
+)
 def test(session):
     session.install("coverage", "pytest")
     session.install(".")
     session.run("coverage", "run", "-m", "pytest", "-x", "-m", "not slow")
     session.run("coverage", "html", "--skip-empty", "--omit=dmerk/test/*")
-    print("Coverage HTML Report: " + "file://" + str(pathlib.Path("htmlcov/index.html").absolute()))
+    print(
+        "Coverage HTML Report: "
+        + "file://"
+        + str(pathlib.Path("htmlcov/index.html").absolute())
+    )
+
 
 @nox.session()
 def profile(session):
