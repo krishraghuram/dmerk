2644e009ad548f8c38ee13888607eb8eb5d18271
Raghuram Krishnaswami <krish.raghuram@gmail.com>
2023-05-24 18:44:50 -0700

    Fixed bug in compare

--- dmerk/compare.py
+++ dmerk/compare.py
@@ -34,8 +34,9 @@ def _get_unmatched_files(matching_paths, unmatched_merkle):
     unmatched_files = []
     for k, v in unmatched_merkle.items():
         if v["_type"] == "file":
-            # Ensure that no parent directory of unmatched file k is present in matching_paths
-            if len(set(k.parents) & set(matching_paths)) == 0:
+            # Ensure that unmatched_file k is not already present in matching_paths
+            # Also ensure that no parent directory of unmatched file k is present in matching_paths
+            if len(set(itertools.chain(k.parents, [k])) & set(matching_paths)) == 0:
                 unmatched_files.append(k)
         elif v["_type"] == "directory":
             unmatched_files.extend(_get_unmatched_files(matching_paths, v["_children"]))
@@ -43,7 +44,11 @@ def _get_unmatched_files(matching_paths, unmatched_merkle):
 
 
 def compare(
-    merkle_1, merkle_2, unmatched_parent_merkle_1=None, unmatched_parent_merkle_2=None
+    merkle_1,
+    merkle_2,
+    unmatched_parent_merkle_1=None,
+    unmatched_parent_merkle_2=None,
+    matches=None,
 ):
     """
     Compare two directory merkle trees and return the matches and diffs
@@ -55,6 +60,8 @@ def compare(
         unmatched_parent_merkle_1 = {}
     if unmatched_parent_merkle_2 is None:
         unmatched_parent_merkle_2 = {}
+    if matches is None:
+        matches = []
     digest_to_paths_1 = _get_digest_to_paths_map(unmatched_parent_merkle_1 | merkle_1)
     digest_to_paths_2 = _get_digest_to_paths_map(unmatched_parent_merkle_2 | merkle_2)
     digest_set_1 = _get_digest_set(unmatched_parent_merkle_1 | merkle_1)
@@ -62,10 +69,12 @@ def compare(
     matching_digests = digest_set_1 & digest_set_2
     exclusive_digests_1 = digest_set_1 - matching_digests
     exclusive_digests_2 = digest_set_2 - matching_digests
-    matches = [
-        (digest_to_paths_1[digest], digest_to_paths_2[digest])
-        for digest in matching_digests
-    ]
+    matches.extend(
+        [
+            (digest_to_paths_1[digest], digest_to_paths_2[digest])
+            for digest in matching_digests
+        ]
+    )
     unmatched_merkle_1 = _get_unmatched_merkle(
         unmatched_parent_merkle_1 | merkle_1, digest_to_paths_1, exclusive_digests_1
     )
@@ -84,7 +93,11 @@ def compare(
     )
     if len(children_merkle_1) > 0 and len(children_merkle_2) > 0:
         children_matches, unmatched_files_1, unmatched_files_2 = compare(
-            children_merkle_1, children_merkle_2, unmatched_merkle_1, unmatched_merkle_2
+            children_merkle_1,
+            children_merkle_2,
+            unmatched_merkle_1,
+            unmatched_merkle_2,
+            matches,
         )
         matches.extend(children_matches)
     else:
