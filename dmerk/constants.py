5a342f770cf3a8149bbcbe33dab81f7e65be50da
Raghuram Krishnaswami <krish.raghuram@gmail.com>
2023-11-02 20:57:48 -0700

    Refactored constants

--- dmerk/constants.py
+++ dmerk/constants.py
@@ -6,35 +6,27 @@ PYPI_PACKAGE_NAME = __package__  # Lucky!!!
 PYPI_VERSION = version(PYPI_PACKAGE_NAME)
 PYPI_AUTHOR_EMAIL = metadata(PYPI_PACKAGE_NAME)["Author-email"]
 
-
-# Ref: https://stackoverflow.com/questions/880530
-def __getattr__(name):
-    if name == "APP_STATE_PATH":
-        return str(
-            platformdirs.user_state_path(
-                appname=PYPI_PACKAGE_NAME,
-                appauthor=PYPI_AUTHOR_EMAIL,
-                version=PYPI_VERSION,
-                ensure_exists=True,
-            )
-        )
-    elif name == "APP_LOG_PATH":
-        return str(
-            platformdirs.user_log_path(
-                appname=PYPI_PACKAGE_NAME,
-                appauthor=PYPI_AUTHOR_EMAIL,
-                version=PYPI_VERSION,
-                ensure_exists=True,
-            )
-        )
-    elif name == "APP_CONFIG_PATH":
-        return str(
-            platformdirs.user_config_path(
-                appname=PYPI_PACKAGE_NAME,
-                appauthor=PYPI_AUTHOR_EMAIL,
-                version=PYPI_VERSION,
-                ensure_exists=True,
-            )
-        )
-    else:
-        AttributeError(f"module '{__name__}' has no attribute '{name}'")
+APP_STATE_PATH = str(
+    platformdirs.user_state_path(
+        appname=PYPI_PACKAGE_NAME,
+        appauthor=PYPI_AUTHOR_EMAIL,
+        version=PYPI_VERSION,
+        ensure_exists=True,
+    )
+)
+APP_LOG_PATH = str(
+    platformdirs.user_log_path(
+        appname=PYPI_PACKAGE_NAME,
+        appauthor=PYPI_AUTHOR_EMAIL,
+        version=PYPI_VERSION,
+        ensure_exists=True,
+    )
+)
+APP_CONFIG_PATH = str(
+    platformdirs.user_config_path(
+        appname=PYPI_PACKAGE_NAME,
+        appauthor=PYPI_AUTHOR_EMAIL,
+        version=PYPI_VERSION,
+        ensure_exists=True,
+    )
+)
