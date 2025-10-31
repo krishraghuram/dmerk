krishraghuram/dmerk

commit d69c07955d5973c14e85f1a202d87545c7d234b8

Fixed bug in CompareWidget.on_resize. Added await on _reset_to_filepicker, just to be safe Minor refactoring and style cleanup
There was a race condition between events: Resize and
Unmount (triggered from _reset_to_filepicker)
which lead to exception like
NoMatches: No nodes match 'Horizontal' on CompareWidget(id='compare-left')

Fix was essentially just removing the handling of Resize event, we dont need this anymore
Didnt look deeper into why we added this and why this is no longer needed,
but did quick verification and everything seems to be good.

dmerk/tui/widgets/compare_widget.py
@@ -53,6 +53,8 @@ class Columns(Enum):
 
 class CompareWidget(Widget):
 
+    BUTTON_RESET_COMPARE = "button-reset-compare"
+
     merkle_subpath: reactive[PurePath | None] = reactive(None)
     prev_cell_key = None
     filter_by = reactive("")
@@ -80,13 +82,13 @@ class CompareWidget(Widget):
         else:
             raise ValueError(f"path {path} must be a dmerk file")
 
-    def _reset_to_filepicker(self) -> None:
+    async def _reset_to_filepicker(self) -> None:
         from dmerk.tui.widgets.file_picker import FilePicker
 
         id_ = self.id.split("-")[-1] if self.id else ""
         id_ = "-".join(["filepicker", id_])
         cast(Widget, self.parent).mount(FilePicker(id=id_), after=self)
-        self.remove()
+        await self.remove()
 
     @work(thread=True)
     async def _main(self, path: Path) -> None:
@@ -107,12 +109,12 @@ class CompareWidget(Widget):
             yield Vertical(
                 Horizontal(Label(Text(f"{self.merkle.path}", style="bold"))),
                 compare_table,
-                Button("RESET", "primary", id="button-reset-compare"),
+                Button("RESET", "primary", id=self.BUTTON_RESET_COMPARE),
             )
 
-    def on_button_pressed(self, message: Button.Pressed) -> None:
-        if message.button.id == "button-reset-compare":
-            self._reset_to_filepicker()
+    async def on_button_pressed(self, message: Button.Pressed) -> None:
+        if message.button.id == self.BUTTON_RESET_COMPARE:
+            await self._reset_to_filepicker()
 
     def on_data_table_cell_selected(self, message: DataTable.CellSelected) -> None:
         if "NAME" in message.cell_key:
@@ -141,9 +143,6 @@ class CompareWidget(Widget):
         await self._refresh()
 
     async def watch_merkle_subpath(self) -> None:
-        await self._refresh()
-
-    async def on_resize(self, event: Resize) -> None:
         await self._refresh()
 
     async def _refresh(self) -> None:

dmerk/tui/__init__.py
@@ -51,6 +51,8 @@ class DmerkApp(App[None]):
         ("d", "toggle_dark", "Toggle dark mode"),
     ]
 
+    BUTTON_GENERATE = "button-generate"
+
     def on_ready(self, event: Ready) -> None:
         root_logger = logging.getLogger()
         root_logger.setLevel(logging.DEBUG)
@@ -77,6 +79,7 @@ class DmerkApp(App[None]):
                         Button(
                             str.upper(Tabs.Generate.name),
                             variant="primary",
+                            id=self.BUTTON_GENERATE,
                         ),
                         id="bottom",
                     ),
@@ -100,7 +103,7 @@ class DmerkApp(App[None]):
         merkle.save(filename=filename)
 
     def on_button_pressed(self, message: Button.Pressed) -> None:
-        if message.button.id == "button-generate":
+        if message.button.id == self.BUTTON_GENERATE:
             highlighted_path = self.query_one(FileManager).highlighted_path
             if highlighted_path is not None:
                 if highlighted_path.is_dir():

dmerk/tui/styles.tcss
@@ -77,14 +77,14 @@ Screen #tab-compare {
         }
         CompareWidget {
             &:first-child {
-                margin: 0 0 0 2; /* left: 2 */
+                margin-left: 2;
             }
             &:last-child {
-                margin: 0 2 0 0; /* right: 2 */
+                margin-right: 2;
             }
             width: 1fr;
             Horizontal { /* CompareWidget Label */
-                margin: 0 2 0 0;
+                margin-right: 2;
                 background: $panel-lighten-2;
                 width: 100%;
                 height: auto;

commit 637d3db228c20d4a5b1c748401855e0c11cabe46

Fixed bug that was caused in commit: d69c0795
Removing _refresh on Resize event meant that a race condition from long ago resurfaced
This race condition happened when self.size.width is still 0 in _refresh_table,
which meant that CompareWidget._get_column_width didnt work as expected,
and lead to basically 0 width columns

dmerk/tui/widgets/compare_widget.py
@@ -99,9 +99,15 @@ class CompareWidget(Widget):
             self.loading = False
             await self.recompose()
             await self._add_watches()
-            await self._refresh()
+            self.call_after_refresh(self._refresh_when_ready)
         elif event.state in [WorkerState.ERROR, WorkerState.CANCELLED]:
             raise Exception("Worker failed/cancelled")
+
+    async def _refresh_when_ready(self) -> None:
+        if self.size.width > 0:
+            await self._refresh()
+        else:
+            self.call_after_refresh(self._refresh_when_ready)
 
     def compose(self) -> ComposeResult:
         if not self.loading: