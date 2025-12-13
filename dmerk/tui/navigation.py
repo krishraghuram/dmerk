089cdc9bd4c71cacc2b62afee79eef74c8490ceb
Raghuram Krishnaswami <krish.raghuram@gmail.com>
2025-12-13 09:27:41 -0800

    WIP 15: Allow ctrl+<direction> for forced keyboard navigation

--- dmerk/tui/navigation.py
+++ dmerk/tui/navigation.py
@@ -4,11 +4,50 @@ from typing import Any, cast
 
 from textual.app import App
 from textual.css.query import NoMatches
-from textual.events import Key
+from textual.events import Key as TextualKeyEvent
 from textual.geometry import Offset, Region, Size
 from textual.widget import Widget
 from textual.widgets import DataTable, Input
 from textual.widgets._tabbed_content import ContentTabs
+
+
+class Key(Enum):
+    UP = "Up"
+    DOWN = "Down"
+    LEFT = "Left"
+    RIGHT = "Right"
+    CTRL_UP = "CtrlUp"
+    CTRL_DOWN = "CtrlDown"
+    CTRL_LEFT = "CtrlLeft"
+    CTRL_RIGHT = "CtrlRight"
+
+    @classmethod
+    def from_event(cls, event: TextualKeyEvent) -> "Key":
+        key = event.key
+        match key:
+            case "ctrl+up":
+                return cls.CTRL_UP
+            case "ctrl+down":
+                return cls.CTRL_DOWN
+            case "ctrl+left":
+                return cls.CTRL_LEFT
+            case "ctrl+right":
+                return cls.CTRL_RIGHT
+            case "up":
+                return cls.UP
+            case "down":
+                return cls.DOWN
+            case "left":
+                return cls.LEFT
+            case "right":
+                return cls.RIGHT
+            case _:
+                raise ValueError(f"Unexpected value {key=} in Key.from_event")
+
+    def has_ctrl(self):
+        cls = self.__class__
+        ctrl_keys = (cls.CTRL_UP, cls.CTRL_DOWN, cls.CTRL_LEFT, cls.CTRL_RIGHT)
+        return self in ctrl_keys
 
 
 class Direction(Enum):
@@ -18,17 +57,16 @@ class Direction(Enum):
     RIGHT = "Right"
 
     @classmethod
-    def from_key(cls, key: str) -> "Direction":
-        if key == "up":
-            return cls.UP
-        elif key == "down":
-            return cls.DOWN
-        elif key == "left":
-            return cls.LEFT
-        elif key == "right":
-            return cls.RIGHT
-        else:
-            raise ValueError(f"Unexpected value {key=} in Direction.from_key")
+    def from_key(cls, key: Key) -> "Direction":
+        match key:
+            case Key.UP | Key.CTRL_UP:
+                return cls.UP
+            case Key.DOWN | Key.CTRL_DOWN:
+                return cls.DOWN
+            case Key.LEFT | Key.CTRL_LEFT:
+                return cls.LEFT
+            case Key.RIGHT | Key.CTRL_RIGHT:
+                return cls.RIGHT
 
 
 class NavigationMixin:
@@ -115,15 +153,17 @@ class NavigationMixin:
 
         return navigate
 
-    def on_key(self, event: Key) -> None:
+    def on_key(self, event: TextualKeyEvent) -> None:
         assert isinstance(self, App)
 
         try:
-            direction = Direction.from_key(event.key)
+            key = Key.from_event(event)
+            direction = Direction.from_key(key)
+            force_navigate = key.has_ctrl()
             source = self.focused
             if source is None:
                 raise ValueError("source is None!!!")
-            if self.should_navigate(direction, source):
+            if force_navigate or self.should_navigate(direction, source):
                 target = self.ray_trace(direction, source)
                 target.focus()
                 logging.debug(f"{direction=}, {source=}, {target=}")
