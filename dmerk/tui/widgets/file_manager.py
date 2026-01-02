0290e0fd78c71cef5dd3bfb65a6c8dfa0979267f
Raghuram Krishnaswami <krish.raghuram@gmail.com>
2026-01-01 22:26:36 -0800

    Improvement: Don't need to calculate height ourselves, we can let CSS handle that using fractional units

--- dmerk/tui/widgets/file_manager.py
+++ dmerk/tui/widgets/file_manager.py
@@ -149,11 +149,6 @@ class FileManager(Widget):
                 key=str(file),
                 height=3,
             )
-        # Prevent Vertical Scrollbar
-        # Logic is, we set files_table height to self height - sum of heights of other widgets
-        files_table.styles.height = self.size.height - sum(
-            [w.size.height for w in self.query_children() if w != files_table]
-        )
 
     async def on_resize(self, event: Resize) -> None:
         await self._refresh()
